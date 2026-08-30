8b7e2da9d594f91f65108dc8e5767839c01be201
ddorfma2-sys <ddorfma2@asu.edu>
2026-08-30 13:23:07 -0700

    Add Song class with title, artist, and duration
    adding song class

---
+++ 460 Example/Song.java
@@ -0,0 +1,23 @@
+public class Song {
+    private String title;
+    private String artist;
+    private int durationSeconds;
+
+    public Song(String title, String artist, int durationSeconds) {
+        this.title = title;
+        this.artist = artist;
+        this.durationSeconds = durationSeconds;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+}
