73f28df2b8d3e12d1e4a899bd33354a381b8dd0d
ddorfma2-sys <ddorfma2@asu.edu>
2026-08-30 13:24:49 -0700

    Create Playlist class to manage songs

---
+++ 460 Example/Playlist.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private String name;
+    private ArrayList<Song> songs;
+
+    public Playlist(String name) {
+        this.name = name;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public int getSongCount() {
+        return songs.size();
+    }
+}
